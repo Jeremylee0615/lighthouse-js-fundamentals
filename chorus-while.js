aa77c7c941cf14ca8fe3c34053212700c9185e3a
Jeremy Lee <jeremylee0615@gmail.com>
2023-01-28 17:57:58 -0800

    Use a while loop to repeat a chorus line

---
+++ chorus-while.js
@@ -0,0 +1,20 @@
+const chorus = "Everything goes on";
+
+let repeat = 0;
+
+while (repeat < 8) {
+   if (repeat ===5){
+	console.log("*Bpm changes*");
+
+}
+
+console.log(chorus)
+       repeat++;
+
+}
+
+console.log("Let's pretend little longer")
+
+
+
+
